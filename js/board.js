f3bc9d2a4668636d018b01fa1cf61c0acc0f3fef
Gregory Kan <kan.gregory@gmail.com>
2015-04-30 10:53:58 +1200

    added board object and initial html/css

---
+++ js/board.js
@@ -0,0 +1,26 @@
+
+
+function Board () 
+{
+	this.sideLength = 10;
+}
+
+Board.prototype.Render = function () 
+{
+	for (var r = 0; r < this.sideLength; r++)
+	{
+		for (var c = 0; c < this.sideLength; c++)
+		{
+			$('.board').append('<div class="cell" id="r_' + r + '-c_' + c +'"></div>');
+		}
+	}
+
+}
+
+// driver code
+
+$(document).ready(function () 
+	{
+		var board = new Board();
+		board.Render();
+	});
